066fef9b03d581915830d5a8a2f7f60f0a0dfff9
LucianoCapogrosso <119363321+LucianoCapogrosso@users.noreply.github.com>
2023-07-09 20:36:22 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 
-function myFunction(imgs) {
 
-  // Get the expanded image
 
+let arraydatos=[["./images/2023/SJR X TORNADOS-1.jpg","./images/2023/SJR X TORNADOS-2.jpg","./images/2023/SJR X TORNADOS-3.jpg","./images/2023/SJR X TORNADOS-4.jpg","./images/2023/SJR X TORNADOS-5.jpg","./images/2023/SJR X TORNADOS-6.jpg","./images/2023/SJR X TORNADOS-7.jpg","./images/2023/SJR X TORNADOS-8.jpg","./images/2023/SJR X TORNADOS-9.jpg","./images/2023/SJR X TORNADOS-10.jpg"],
+["./images/2022/SJR X TORNADOS-1.jpg","./images/2022/SJR X TORNADOS-2.jpg","./images/2022/SJR X TORNADOS-3.jpg","./images/2022/SJR X TORNADOS-4.jpg","./images/2022/SJR X TORNADOS-5.jpg","./images/2022/SJR X TORNADOS-6.jpg","./images/2022/SJR X TORNADOS-7.jpg","./images/2022/SJR X TORNADOS-8.jpg","./images/2022/SJR X TORNADOS-9.jpg","./images/2022/SJR X TORNADOS-10.jpg"],
+["./images/2021/SJR X TORNADOS-1.jpg","./images/2021/SJR X TORNADOS-2.jpg","./images/2021/SJR X TORNADOS-3.jpg","./images/2021/SJR X TORNADOS-4.jpg","./images/2021/SJR X TORNADOS-5.jpg","./images/2021/SJR X TORNADOS-6.jpg","./images/2021/SJR X TORNADOS-7.jpg","./images/2021/SJR X TORNADOS-8.jpg","./images/2021/SJR X TORNADOS-9.jpg","./images/2021/SJR X TORNADOS-10.jpg"]];    
+
+function expandPic(imgs) {
+
+  
   var expandImg = document.getElementById("expandedImg");
 
   // Get the image text
@@ -23,35 +28,56 @@ function myFunction(imgs) {
 
 }
 
-function doPortItem(n) {
-      
+
+function cambia(){
+
+var item = yearsel.value;
+let long = arraydatos[item].length;
+let imagen = document.getElementsByClassName("d-block w-50 img-thumbnail rounded float-start");
+console.log(document.getElementsByClassName("carousel-item"));
+for ( var i =0 ; i <= long ; i++) {
+imagen[i].src = arraydatos[item][i];
+}
+}
+function gallerygenerator() {
+ 
+  let item = yearsel.value;
+  let long = arraydatos[item].length;
+  let imgactive = document.getElementsByName("active");
+  console.log(imgactive);
+  for (var i= 0 ; i <= 3 ; i++) {
+      imgactive[i].src = arraydatos[item][i];
+  };
+ for (var i= 4 ; i <= long ; i=i+4) {
   let img = document.createElement('img');
-
-  img.src = './images/SJR X TORNADOS-' + n + '.jpg';
-
+  let imga = document.createElement('img');
+  let imgb = document.createElement('img');
+  let imgc = document.createElement('img');
+  
+  img.src = arraydatos[item][i];
+  imga.src = arraydatos[item][i+1];
+  imgb.src = arraydatos[item][i+2];
+  imgc.src = arraydatos[item][i+3];
+ 
+ 
   img.addEventListener('load', () => {
 
-      let div = `<div class="col-12 col-lg-12 col-md-12 col-sm-12">
+    let div = `<div class="carousel-item" >
+    <div class="col-12 col-lg-12 col-md-12 col-sm-12" >
+    <img class="d-block w-50 img-thumbnail rounded float-start" src= ${img.src}  alt="demo" onclick="myFunction(this);" onerror="this.src='./images/Tornado.jpg'"/>
+    <img class="d-block w-50 img-thumbnail rounded float-start" src= ${imga.src}  alt="demo" onclick="myFunction(this);" onerror="this.src='./images/Tornado.jpg'"/>
+    <img class="d-block w-50 img-thumbnail rounded float-start" src= ${imgb.src}  alt="demo" onclick="myFunction(this);" onerror="this.src='./images/Tornado.jpg'"/>
+    <img class="d-block w-50 img-thumbnail rounded float-start" src= ${imgc.src}  alt="demo" onclick="myFunction(this);" onerror="this.src='./images/Tornado.jpg'"/>
+    </div>
+    </div>`;
 
-    <div class="columna">
-        
-       <img class="img-thumbnail rounded" src="${img.src}" alt="demo" onclick="myFunction(this);">
-      
-
-   </div>
-
- </div>`;
-
-      document.querySelector('#gallery-items').innerHTML += div;
-
-     if (n < 4 ){ doPortItem(++n);}
-     return ;
-    
-  })
-
+    document.querySelector('#gallery-items').innerHTML += div;
+  
+                                      })
+  }
+     
+  
+  
 }
-function cambia(){
- var a = yearsel.value;
- doPortItem(1);
-  return;
-}
+
+
